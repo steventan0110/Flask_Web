10fa13b2f144a7baf0d2a4d2b9255bd8dbb46fc6
Weiting Tan <wtan12@jhu.edu>
2020-01-15 04:34:06 +0000

    deploy on server

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import json
+import urllib3
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '1A37BbcCJh67'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+
+app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
